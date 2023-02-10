57e701fd06d4615522a626b3eb6152af11f12014
Deyan Peev <deqn.peev@gmail.com>
2023-02-10 22:28:42 +0200

    Palindrome Number

---
+++ Palindrome Number/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(IsPalindrome(121));
+Console.WriteLine(IsPalindrome(1241));
+Console.WriteLine(IsPalindrome(1221));
+
+
+bool IsPalindrome(int x)
+{
+    string asStr = x.ToString();
+    int start = 0;
+    int end = asStr.Length - 1;
+    while(start < end)
+    {
+        if (asStr[start] != asStr[end])
+        {
+            return false;
+        }
+        start++;
+        end--;
+    }
+
+    return true;
+}
