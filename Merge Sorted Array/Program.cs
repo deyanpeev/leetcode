c2d7c9ebf5bd776272163a41186b4926d37dc391
Deyan Peev <deqn.peev@gmail.com>
2023-02-14 09:20:32 +0200

    Merge Sorted Array

---
+++ Merge Sorted Array/Program.cs
@@ -0,0 +1,38 @@
+﻿class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int[] nums1Cloned = nums1.Clone() as int[];
+        int index1 = 0;
+        int index2 = 0;
+        for (int i = 0; i < nums1.Length; i++)
+        {
+            if(index1 >= m)
+            {
+                nums1[i] = nums2[index2++];
+                continue;
+            }
+            if(index2 >= n)
+            {
+                nums1[i] = nums1Cloned[index1++];
+                continue;
+            }
+            if (nums1Cloned[index1] < nums2[index2])
+            {
+                nums1[i] = nums1Cloned[index1++];
+            }
+            else
+            {
+                nums1[i] = nums2[index2++];
+            }
+        }
+    }
+}
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        
+    }
+}
