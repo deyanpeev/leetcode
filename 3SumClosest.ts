319ea1361451520ba1620898a8f27e56ac12d8aa
Deyan Peev <deqn.peev@gmail.com>
2023-02-18 23:26:07 +0200

    Optimization

--- 3SumClosest.ts
+++ 3SumClosest.ts
@@ -2,7 +2,7 @@ function threeSumClosest(nums: number[], target: number): number {
   let closest = Number.MAX_VALUE
   nums = nums.sort((a,b) => a-b)
 
-  for(let i = 0; i < nums.length; i++) {
+  for(let i = 0; i < nums.length-2; i++) {
     let left = i + 1;
     let right = nums.length - 1;
 
@@ -27,9 +27,10 @@ function threeSumClosest(nums: number[], target: number): number {
 };
 
 function isSmallerDistance(current: number, previous: number, target: number): boolean {
-  const diffCurrent = current * target < 0 ? Math.abs(current * -1 + target) : Math.abs(current - target);
-  const diffPrevious = previous * target < 0 ? Math.abs(previous * -1 + target) : Math.abs(previous - target);
-  return diffCurrent < diffPrevious;
+  // const diffCurrent = current * target < 0 ? Math.abs(current * -1 + target) : Math.abs(current - target);
+  // const diffPrevious = previous * target < 0 ? Math.abs(previous * -1 + target) : Math.abs(previous - target);
+  // return diffCurrent < diffPrevious;
+  return (Math.abs(current-target)<Math.abs(previous-target));
 }
 
 console.log(threeSumClosest([0,0,0], 1));
