cf9c03d47f5996a9c4b0cf3699c87e8aad283d47
Deyan Peev <deqn.peev@gmail.com>
2023-02-20 10:10:24 +0200

    with content

---
+++ Letter Combinations of a Phone Number.ts
@@ -0,0 +1,24 @@
+const LETTERS = {
+  '2': ['a', 'b', 'c'],
+  '3': ['d', 'e', 'f'],
+  '4': ['g', 'h', 'i'],
+  '5': ['j', 'k', 'l'],
+  '6': ['m', 'n', 'o'],
+  '7': ['p', 'q', 'e', 's'],
+  '8': ['t', 'u', 'v'],
+  '9': ['w', 'x', 'y', 'z'],
+}
+function letterCombinations(digits: string): string[] {
+  const result = [];
+  for(let i = 0; i < digits.length; i++) {
+
+  }
+
+  return result;
+};
+
+function recursiveCombinations(): string[] {
+  
+}
+
+console.log(letterCombinations('ab'));
