b83c2c8a0f219b4609cb96b3fbf27eb3b352d2a7
Deyan Peev <deqn.peev@gmail.com>
2023-02-20 15:04:32 +0200

    Letter combination

--- Letter Combinations of a Phone Number.ts
+++ Letter Combinations of a Phone Number.ts
@@ -1,24 +1,50 @@
-const LETTERS = {
-  '2': ['a', 'b', 'c'],
-  '3': ['d', 'e', 'f'],
-  '4': ['g', 'h', 'i'],
-  '5': ['j', 'k', 'l'],
-  '6': ['m', 'n', 'o'],
-  '7': ['p', 'q', 'e', 's'],
-  '8': ['t', 'u', 'v'],
-  '9': ['w', 'x', 'y', 'z'],
-}
 function letterCombinations(digits: string): string[] {
-  const result = [];
-  for(let i = 0; i < digits.length; i++) {
-
+  if(!digits || !digits.length) {
+    return [];
+  }  
+  let result: string[] = getDigitsByLetter(digits[0]);
+  for(let i = 1; i < digits.length; i++) {
+    result = getNextLetterCombCombinations(result, getDigitsByLetter(digits[i]));
   }
 
   return result;
 };
 
-function recursiveCombinations(): string[] {
-  
+function getNextLetterCombCombinations(currentArray: string[], letters: string[]): string[] {
+  const resultArray: string[] = [];
+
+  let element: string | undefined = currentArray.shift();
+  while(element) {
+    for(let letter of letters) {
+      resultArray.push(element+letter);
+    }
+    element = currentArray.shift();
+  }
+
+  return resultArray;
 }
 
-console.log(letterCombinations('ab'));
+function getDigitsByLetter(letter: string): string[] {
+  switch(letter) {
+    case '2':
+      return ['a', 'b', 'c'];
+    case '3':
+      return ['d', 'e', 'f'];
+    case '4':
+      return ['g', 'h', 'i'];
+    case '5':
+      return ['j', 'k', 'l'];
+    case '6':
+      return ['m', 'n', 'o'];
+    case '7':
+      return ['p', 'q', 'r', 's'];
+    case '8':
+      return ['t', 'u', 'v'];
+    case '9':
+      return ['w', 'x', 'y', 'z'];
+    default:
+      return [];
+  }
+}
+
+console.log(letterCombinations('222'));
