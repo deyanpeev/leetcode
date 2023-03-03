9bd28a01e59f3e2bfac0e7474b18d724fb1aa9d4
Deyan Peev <deqn.peev@gmail.com>
2023-03-03 23:55:17 +0200

    Pascal init

---
+++ 118.Pascal's Triangle/Pascal Triangle.ts
@@ -0,0 +1,11 @@
+function generate(numRows: number): number[][] {
+  const result: number[][] = [];
+  for(let i = 1; i <= numRows; i++) {
+    let currentArr: number[] = [];
+    for(let j = 1; j <= i; j++) {
+      
+    }
+  }
+
+  return result;
+};
