1f7be76e8b00e8a085640e646a7114d6d51a256d
Deyan Peev <deqn.peev@gmail.com>
2023-03-05 17:26:09 +0200

    Optimization stock calc

--- 121. Best Time to Buy and Sell Stock/MaxProfit.go
+++ 121. Best Time to Buy and Sell Stock/MaxProfit.go
@@ -1,14 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func maxProfit(prices []int) int {
 	max := 0
 	for i := 0; i < len(prices)-1; i++ {
 		for j := i + 1; j < len(prices); j++ {
-			currentDiffeerence := prices[j] - prices[i]
-			if currentDiffeerence > max {
-				max = currentDiffeerence
+			currentDifference := prices[j] - prices[i]
+			if currentDifference > max {
+				max = currentDifference
 			}
 		}
 	}
@@ -16,8 +18,25 @@ func maxProfit(prices []int) int {
 	return max
 }
 
+func maxProfitOptimized(prices []int) int {
+	buy, sell, max := 0, 1, 0
+	for sell < len(prices) {
+		currentDifference := prices[sell] - prices[buy]
+		if currentDifference > 0 {
+			if max < currentDifference {
+				max = currentDifference
+			}
+		} else {
+			buy = sell
+		}
+		sell++
+	}
+
+	return max
+}
+
 func main() {
 	arr := []int{7, 1, 5, 3, 6, 4}
-	max := maxProfit(arr)
+	max := maxProfitOptimized(arr)
 	fmt.Println(max)
 }
