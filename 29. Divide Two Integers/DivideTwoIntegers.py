ae31fc8c559e58919dddb4f6ec8e257ee205ec25
Deyan Peev <deqn.peev@gmail.com>
2023-03-26 23:46:17 +0300

    DivideTwoIntegers finished

--- 29. Divide Two Integers/DivideTwoIntegers.py
+++ 29. Divide Two Integers/DivideTwoIntegers.py
@@ -1,17 +1,25 @@
 class Solution:
   def divide(self, dividend: int, divisor: int) -> int:
+    MAX_VALUE = (1 << 31) - 1
+    MIN_VALUE = (1<<31) * -1
+
     result = 0
-    MAX_VALUE = 1 << 31 - 1
     isPositive = (dividend > 0) == (divisor > 0)
     dividend, divisor = abs(dividend), abs(divisor)
     while dividend >= divisor:
       powerToAdd = 0
-      while dividend > (divisor<<(1+powerToAdd)):
+      while dividend >= (divisor<<(1+powerToAdd)):
         powerToAdd += 1
       result+=1<<powerToAdd
       dividend-=divisor<<powerToAdd
 
-    return result if isPositive else -result
+    result = result if isPositive else -result
+    if result > MAX_VALUE:
+      return MAX_VALUE
+    if result < MIN_VALUE:
+      return MIN_VALUE
+
+    return result
 
 s = Solution()
-print(s.divide(10, 5))
+print(s.divide(-2147483648, 1))
